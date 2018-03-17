834796bed8d1d2845ed98203e1204154a106b21f
kautuk <kautukkundan@gmail.com>
2018-03-17 22:54:20 +0530

    zipaligned code

--- app.js
+++ app.js
@@ -6,27 +6,19 @@ var multer  = require('multer')
 var upload = multer({ dest: 'tmp/' })
 
 function encrypt(file, org){
-
   var toBeEnc = fs.readFileSync('./tmp/'+file).toString("hex");
-
   var encrypted = CryptoJS.AES.encrypt(toBeEnc, '12334');
-
   fs.appendFile('./safe/'+org+'.Krypt', encrypted, function (err) {
-    if (err) {
-                // append failed
-              } else {
-                // done
-              }
-            });
+    if (err) { } 
+    else { }
+  });
 }
 
 function decrypt(file) {
  var file2b = fs.readFileSync('./tmp/'+ file);
  var bytes  = CryptoJS.AES.decrypt(file2b.toString(), '12334');
- // console.log(bytes.toString(CryptoJS.enc.Utf8))
  return bytes.toString(CryptoJS.enc.Utf8)
 }
-
 
 app.post("/encrypt", upload.single('enc'), function (req, res, next) {  
   var org = req.file.originalname;
@@ -51,14 +43,9 @@ app.post("/decrypt", upload.single('dec'), function (req, res, next) {
   }, 3000);
 });
 
-
 app.use("/download",function(req,res){
-  console.log(data);
   res.render('dl', {clickhandler : "createFileFromHex("+"'"+data+"'"+","+"'"+ org.slice(0,-6)+"')"})
 })
-
-
-
 
 app.use(express.static(__dirname+"/public"));
 app.set("view engine","ejs");
@@ -68,33 +55,12 @@ app.get('/token',function(req,res){
 })
 
 app.get('/',function (req, res) {
-
   res.render("main")
-    // console.log(coa);
-    // //Encrypt
-    
-    
-  //  // Decrypt
-
-
-     // // Decrypt
-   //  var bytes  = CryptoJS.AES.decrypt(encrypted.toString(), '12334');
-   //  console.log(bytes.toString(CryptoJS.enc.Utf8));
-   //  if(coa===bytes.toString(CryptoJS.enc.Utf8)){
-   //      console.log("hurray!!!");
-   //      fs.appendFile('./files/COA.txt',coa, function (err) {
-   //          if (err) {
-   //              // append failed
-   //          } else {
-   //              // done
-   //          }
-   //      })
-   //  }
-   //  if(coa===bytes.toString())
-   //  res.render("index",{clickhandler : "createFileFromHex("+"'"+coa+"'"+", 'rikki.png')"});
-
-
  });
+
+app.get('/store',function (req, res) {
+  res.render("store")
+});
 
 app.listen(3000,function () {
  console.log("HINT started on port 3000");
