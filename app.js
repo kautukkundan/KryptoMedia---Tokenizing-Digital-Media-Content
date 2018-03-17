b502bf32a546a9e3ada57dff5c1e037bbbbb58d1
kautuk <kautukkundan@gmail.com>
2018-03-17 15:59:42 +0530

    testing encryption

--- app.js
+++ app.js
@@ -4,6 +4,20 @@ var fs = require('fs');
 var CryptoJS = require("crypto-js");
 
 
+function encrypt(file){
+
+  var toBeEnc = fs.readFileSync(file).toString("hex");
+
+  var encrypted = CryptoJS.AES.encrypt(toBeEnc, '12334');
+
+  fs.appendFile('./files/'+file+'.Krypt', encrypted, function (err) {
+    if (err) {
+                // append failed
+              } else {
+                // done
+              }
+            });
+}
 
 
 app.use(express.static(__dirname+"/public"));
@@ -11,28 +25,31 @@ app.set("view engine","ejs");
 
 
 app.get('/',function (req, res) {
-   var coa = fs.readFileSync('./files/COA.png').toString("hex");
-   console.log(coa);
-   //Encrypt
-    var encrypted = CryptoJS.AES.encrypt(coa, '12334');
-   // Decrypt
-    var bytes  = CryptoJS.AES.decrypt(encrypted.toString(), '12334');
-    console.log(bytes.toString(CryptoJS.enc.Utf8));
-    if(coa===bytes.toString(CryptoJS.enc.Utf8)){
-        console.log("hurray!!!");
-        fs.appendFile('./files/COA.txt',coa, function (err) {
-            if (err) {
-                // append failed
-            } else {
-                // done
-            }
-        })
-    }
-    res.render("index",{clickhandler : "createFileFromHex("+"'"+coa+"'"+", 'rikki.png')"});
+
+  res.render("main")
+    // console.log(coa);
+    // //Encrypt
+    
+    
+  //  // Decrypt
+  //  var file2b = fs.readFileSync('./files/test.Krypt');
+  //  var bytes  = CryptoJS.AES.decrypt(file2b.toString(), '12334');
+  //  console.log(bytes.toString(CryptoJS.enc.Utf8));
+  //  if(coa===bytes.toString(CryptoJS.enc.Utf8)){
+  //   console.log("hurray!!!");
+  //   fs.appendFile('./files/COA.txt',coa, function (err) {
+  //     if (err) {
+  //               // append failed
+  //             } else {
+  //               // done
+  //             }
+  //           })
+  //   res.render("index",{clickhandler : "createFileFromHex("+"'"+ coa +"'"+", 'rikki.png')"});
+  // }
 
 
 });
 
 app.listen(3000,function () {
-   console.log("HINT started on port 3000");
+ console.log("HINT started on port 3000");
 });
