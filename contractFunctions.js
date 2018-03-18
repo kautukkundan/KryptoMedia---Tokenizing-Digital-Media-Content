7a75aeac0d61cfea414a74155f47f3c6592d8481
vaibhavchellani <vaibhavchellani223@gmail.com>
2018-03-18 09:58:27 +0530

    added

--- contractFunctions.js
+++ contractFunctions.js
@@ -1,6 +1,8 @@
 var express     =   require('express');
 var app         =   express();
 
+app.use(express.static(__dirname+"/public"));
+app.set("view engine","ejs");
 
 var Web3 = require('web3');
 web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
@@ -12,7 +14,7 @@ let CoreObj = require('./trufflehin/build/contracts/Core.json');
 //let obj2 = require('./trufflehin/build/contracts/Test.json')
 
 var DeployerContractABI = JSON.stringify(CoreDeployerObj.abi);
-var DeployerContractAddress = obj.networks['5777'].address;
+var DeployerContractAddress = CoreDeployerObj.networks['5777'].address;
 // var DeployerContractBytecode = obj.bytecode
 var DeployerContract = web3.eth.contract(JSON.parse(DeployerContractABI));
 var CoreDeployer = DeployerContract.at(DeployerContractAddress);
@@ -21,7 +23,14 @@ var CoreContractABI = JSON.stringify(CoreObj.abi);
 var CoreContract = web3.eth.contract(JSON.parse(CoreContractABI));
 
 function returnContractInstance(_symbol){
-    var CoreInstanceAddress = getAddressFromCores(_symbol);
+    var CoreInstanceAddress = CoreDeployer.getAddressFromCores(_symbol, function (err, result) {
+            if(err){
+                console.log(err);
+            } else {
+                console.log(result);
+                return result;
+            }
+        })
     var CoreInstance = CoreContract.at(CoreInstanceAddress);
     return CoreInstance;
 }
@@ -34,6 +43,7 @@ app.get("/createAsset/:sym/:name",function (req, res) {
                 console.log(err);
             }else {
                 console.log(result);
+                res.redirect("http://localhost:3000/up");
             }
         })
     });
@@ -54,14 +64,23 @@ app.get("/getAddressFromCores",function (req, res) {
 
 //app.get("/transfer/:sym/:token/:to")
 
-app.get("/generateAsset",function (req,res) {
+app.get("/generateAsset/:_symbol/:_assetType/:hash/:personalMessage",function (req,res) {
     var _symbol = req.params._symbol;
     var personalMessage = req.params.personalMessage;  
     var hash  = req.params.hash;   
     var _assetType = req.params._assetType;  
         
 
+        // var instance = CoreDeployer.getAddressFromCores(_symbol, function (err, result) {
+        //     if(err){
+        //         console.log(err);
+        //     } else {
+        //         console.log(result);
+        //         return result;
+        //     }
+        // })
         var instance = returnContractInstance(_symbol);
+
         instance._generateAssets(personalMessage,hash,_assetType,function(err,result){
             if(err){
                 console.log(err);
@@ -70,6 +89,7 @@ app.get("/generateAsset",function (req,res) {
                 console.log(result);
             }
         });
+    res.redirect("http://localhost:3000/up");    
 });
 
 app.get("/getOwnerFromId",function (req, res) {
